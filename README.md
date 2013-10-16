14d9d1a5ddb2d437c06b925bfa5bb783474104bc
vhpvmx <vhpvmx@gmail.com>
2013-10-16 13:08:04 +0200

    testing link .git

--- README.md
+++
@@ -1,8 +0,0 @@
-In this repository you can find the code of my thesis project "Parallel Simulation and Visualization of Crowds"
-
-The objetive is to implement and compare different parallel programming approaches to simulate and visualize large crowds on a heterogeneous computing architecture.
-
-Keywords: MPI, CUDA, OpenGL, Ompss, GPU, Crowd
-
-
-
--- mpiStencilCudaV2/mpiStencilCuda.cpp
+++ mpiStencilCudaV2/mpiStencilCuda.cpp
@@ -26,7 +26,10 @@
 //Compilation: make
 //Local Execution: optirun mpirun -np 4 ./mpiStencilCuda
 /*******************************************************************
-usar git
+Instrucciones para subirlo a github: https://help.github.com/articles/create-a-repo
+git commit -a -m "message" //actualiza en el repo todos los archivos modificados o borrados
+git add file //agrega un nuevo archivo al repo
+git push origin master
 envio de mensajes mpi dinamicos
 uso de gpu direct
 hacer pruebas en minotauro
